deb88d02a4fd86e530e419b662160751d68b64af
zzaakiirr <zzaakiirr@gmail.com>
2018-09-26 20:51:25 +0300

    Added fetch_vk_api method witch logging to vk account and returning api to use

---
+++ vk_auth.py
@@ -0,0 +1,18 @@
+import os
+
+import vk
+
+
+def fetch_vk_api():
+    vk_app_id = os.environ.get('VK_APP_ID')
+    vk_login = os.environ.get('VK_LOGIN')
+    vk_password = os.environ.get('VK_PASSWORD')
+
+    session = vk.AuthSession(
+        app_id=vk_app_id,
+        user_login=vk_login,
+        user_password=vk_password
+    )
+    api = vk.API(session, v=5.85, lang='en')
+
+    return api
